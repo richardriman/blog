d64ebabdc199f962ae582a4cab132c1240fefc0f
Joe Sweeney <joe@j1.io>
2016-10-31 17:19:25 -0400

    Login info

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,6 +3,7 @@ defmodule PhoenixBlog.PageController do
 
   def index(conn, _params) do
     logged_in = Addict.Helper.is_logged_in(conn)
-    render conn, "index.html", logged_in: logged_in
+    %{:email => user} = Addict.Helper.current_user(conn)
+    render conn, "index.html", logged_in: logged_in, user: user
   end
 end
