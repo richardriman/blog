a9f016b3a5f00bcce19693381e323711cb431ca3
Joe Sweeney <joe@j1.io>
2016-10-31 19:33:13 -0400

    Styling

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,3 +1,10 @@
 defmodule PhoenixBlog.PostView do
   use PhoenixBlog.Web, :view
+
+  def get_preview(post, count \\ 50) do
+    String.split(post.body, " ")
+    |> Enum.take(count)
+    |> Enum.join(" ")
+    |> Kernel.<> "..."
+  end
 end
