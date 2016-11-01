60d27e85b55f87242769b12e31d91ae69bc0a381
Joe Sweeney <joe@j1.io>
2016-10-31 23:39:17 -0400

    More style

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -7,4 +7,12 @@ defmodule PhoenixBlog.PostView do
     |> Enum.join(" ")
     |> Kernel.<> "..."
   end
+
+  def get_formatted_date(post) do
+    Integer.to_string(post.inserted_at.month)
+    |> Kernel.<> "/"
+    |> Kernel.<> Integer.to_string(post.inserted_at.day)
+    |> Kernel.<> "/"
+    |> Kernel.<> Integer.to_string(post.inserted_at.year)
+  end
 end
