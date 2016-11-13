3802d1bd7735117cfd17e00fbe350a9391e664fb
Joe Sweeney <joe@j1.io>
2016-11-13 15:03:32 -0500

    Begin markdown support

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule PhoenixBlog.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:addict, "~> 0.3"}]
+     {:addict, "~> 0.3"},
+     {:earmark, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,8 +2,11 @@ defmodule PhoenixBlog.PageController do
   use PhoenixBlog.Web, :controller
 
   def index(conn, _params) do
-    logged_in = Addict.Helper.is_logged_in(conn)
-    %{:email => user} = Addict.Helper.current_user(conn)
-    render conn, "index.html", logged_in: logged_in, user: user
+    if (logged_in = Addict.Helper.is_logged_in(conn)) do
+      %{:email => user} = Addict.Helper.current_user(conn)
+      render conn, "index.html", logged_in: logged_in, user: user
+    else
+      render conn, "index.html", logged_in: logged_in, user: "NONE"
+    end
   end
 end
