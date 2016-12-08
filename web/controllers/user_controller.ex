6ee1304bd0d00b7f6d4a3cc981af1911abb82ffa
Joe Sweeney <joe@j1.io>
2016-12-08 00:21:53 -0500

    Consolidate user registration check to function plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,8 +1,20 @@
 defmodule PhoenixBlog.UserController do
   use PhoenixBlog.Web, :controller
   alias PhoenixBlog.User
+  alias PhoenixBlog.Router.Helpers
 
-  plug PhoenixBlog.Registration when action in [:new, :create]
+  plug :check_user_registration when action in [:new, :create]
+
+  def check_user_registration(conn, _opts) do
+    if (Application.get_env(:phoenix_blog, :user_registration) != :enabled) do
+      conn
+      |> put_flash(:error, "Registration is disabled!")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    else 
+      conn
+    end
+  end
 
   def new(conn, _params) do
     changeset = User.changeset(%User{})
