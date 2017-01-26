e2a496554c048fa0eefdf0e9799407a5178d98a0
Joe Sweeney <joe@j1.io>
2017-01-26 00:07:02 -0500

    Add markdown preview to post form

--- brunch-config.js
+++ brunch-config.js
@@ -3,7 +3,6 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
@@ -64,6 +63,7 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {showdown: "showdown"}
   }
 };
