496d1ae9344e572964659f98b00358dd35f186c4
Joe Sweeney <joe@j1.io>
2017-02-02 14:39:06 -0500

    Add tests for creating posts

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -3,8 +3,8 @@ defmodule PhoenixBlog.PostControllerTest do
 
   setup %{conn: conn} = config do
     if username = config[:login_as] do
-      user = insert_user(username: username)
-      conn = assign(conn(), :current_user, user)
+      user = insert_user(%{username: username})
+      conn = assign(build_conn(), :current_user, user)
       {:ok, conn: conn, user: user}
     else
       :ok
@@ -44,5 +44,26 @@ defmodule PhoenixBlog.PostControllerTest do
     posts
     |> Enum.filter(fn post -> post.published == false end)
     |> Enum.each(fn post-> refute String.contains?(conn.resp_body, post.title) end)
+  end
+
+  alias PhoenixBlog.Post
+  @valid_attrs %{title: "test post", body: "some cool text.", published: true}
+  @invalid_attrs %{title: "invalid post"}
+
+  defp post_count(query), do: Repo.one(from p in query, select: count(p.id))
+
+  @tag login_as: "user"
+  test "creates new post and redirects", %{conn: conn} do
+    conn = post(conn, post_path(conn, :create), post: @valid_attrs)
+    assert redirected_to(conn) == post_path(conn, :index)
+    assert Repo.get_by!(Post, @valid_attrs).title == @valid_attrs.title
+  end
+
+  @tag login_as: "user"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = post_count(Post)
+    conn = post(conn, post_path(conn, :create), post: @invalid_attrs)
+    assert html_response(conn, 200) =~ "check the errors"
+    assert post_count(Post) == count_before
   end
 end
