1e76c6d33e44200a02dd1c8fbe9ff3fbe15d724d
Joe Sweeney <joe@j1.io>
2017-02-02 15:10:13 -0500

    Add tests for other post actions

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -5,7 +5,7 @@ defmodule PhoenixBlog.PostControllerTest do
     if username = config[:login_as] do
       user = insert_user(%{username: username})
       conn = assign(build_conn(), :current_user, user)
-      {:ok, conn: conn, user: user}
+      {:ok, conn: conn}
     else
       :ok
     end
@@ -60,10 +60,41 @@ defmodule PhoenixBlog.PostControllerTest do
   end
 
   @tag login_as: "user"
-  test "does not create video and renders errors when invalid", %{conn: conn} do
+  test "does not create post and renders errors when invalid", %{conn: conn} do
     count_before = post_count(Post)
     conn = post(conn, post_path(conn, :create), post: @invalid_attrs)
     assert html_response(conn, 200) =~ "check the errors"
     assert post_count(Post) == count_before
+  end
+
+  @tag login_as: "user"
+  test "edit shows post edit page", %{conn: conn} do
+    post = insert_post(@valid_attrs)
+    conn = get(conn, post_path(conn, :edit, post))
+    assert html_response(conn, :ok) =~ ~r/Edit Post/s
+  end
+
+  @tag login_as: "user"
+  test "updates existing post and redirects", %{conn: conn} do
+    post = insert_post(@valid_attrs)
+    conn = put(conn, post_path(conn, :update, post), post: %{title: "new title"})
+    assert html_response(conn, 302)
+    assert Repo.get(Post, post.id).title == "new title"
+  end
+
+  @tag login_as: "user"
+  test "does not update invlaid post", %{conn: conn} do
+    post = insert_post(@valid_attrs)
+    conn = put(conn, post_path(conn, :update, post), post: %{title: ""})
+    assert html_response(conn, 200) =~ "check the errors"
+    assert Repo.get(Post, post.id).title == post.title
+  end
+
+  @tag login_as: "user"
+  test "deletes existing post", %{conn: conn} do
+    post = insert_post(@valid_attrs)
+    conn = delete(conn, post_path(conn, :delete, post))
+    assert html_response(conn, 302)
+    refute Repo.get(Post, post.id)
   end
 end
