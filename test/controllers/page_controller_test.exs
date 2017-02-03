2c7a697e385aa5ca047116897a552b24ba356416
Joe Sweeney <joe@j1.io>
2017-02-03 15:51:41 -0500

    Test latest post display on home page

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,33 @@
 defmodule PhoenixBlog.PageControllerTest do
   use PhoenixBlog.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
+  test "shows home page", %{conn: conn} do
+    conn = get(conn, "/")
     assert html_response(conn, 200) =~ "John Joseph Sweeney"
   end
+
+  test "handle no post for latest post", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "None yet!"
+  end
+
+  test "only shows latest published post", %{conn: conn} do
+    posts = [
+      %{title: "post 1", body: "this is post 1.", published: true},
+      %{title: "post 2", body: "this is post 2.", published: true},
+      %{title: "post 3", body: "this is post 3.", published: false},
+      %{title: "post 4", body: "this is post 4.", published: false}
+    ]
+    Enum.each(posts, fn post ->
+      insert_post(post)
+      conn = get(conn, "/")
+      if (post.published) do
+        assert html_response(conn, 200) =~ post.title
+      else
+        refute html_response(conn, 200) =~ post.title
+      end
+    end)
+  end
+
+  
 end
