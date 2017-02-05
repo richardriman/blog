c11d4f6ed3516afa7ee088f59822822536ec35d1
Joe Sweeney <joe@j1.io>
2017-02-05 01:43:14 -0500

    Fix deprecated Earmark function

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -3,7 +3,7 @@ defmodule PhoenixBlog.PostView do
   import PhoenixBlog.PostHelpers
 
   def get_formatted_post(post) do
-    Earmark.to_html(post.body)
+    Earmark.as_html!(post.body)
     |> String.replace("<table>", "<table class=\"pure-table\">")
   end
 end
