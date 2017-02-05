6f2576c59ad9ac82f3582ae1ea97b515656352b8
Joe Sweeney <joe@j1.io>
2017-02-05 01:53:02 -0500

    Fix timestamps function in user migration

--- priv/repo/migrations/20161129192109_create_user.exs
+++ priv/repo/migrations/20161129192109_create_user.exs
@@ -7,7 +7,7 @@ defmodule PhoenixBlog.Repo.Migrations.CreateUser do
       add :username, :string, null: false
       add :password_hash, :string
 
-      timestamps
+      timestamps()
     end
 
     create unique_index(:users, [:username])
