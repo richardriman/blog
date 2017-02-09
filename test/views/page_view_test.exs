6284a003d428da939677bdf75f12e04705cab2c2
Joe Sweeney <joe@j1.io>
2017-02-09 14:09:02 -0500

    Add test for home page view function

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,17 @@
 defmodule PhoenixBlog.PageViewTest do
   use PhoenixBlog.ConnCase, async: true
+  import Phoenix.View
+
+  test "renders index.html", %{conn: conn} do
+    post = %PhoenixBlog.Post{
+        title: "test post",
+        slug: "test-post", 
+        body: "this is a test.", 
+        published: true, 
+        inserted_at: ~D[2017-01-01]
+      }
+    content = render_to_string(PhoenixBlog.PageView, "index.html", conn: conn, latest_post: post)
+    assert String.contains?(content, "John Joseph Sweeney")
+    assert String.contains?(content, post.title)
+  end
 end
