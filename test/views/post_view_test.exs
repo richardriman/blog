83fa71399cd4894f85bb726ad368ea775e6fe13d
Joe Sweeney <joe@j1.io>
2017-02-09 13:47:30 -0500

    Add tests for missing post view functions

--- test/views/post_view_test.exs
+++ test/views/post_view_test.exs
@@ -2,6 +2,21 @@ defmodule PhoenixBlog.PostViewTest do
   use PhoenixBlog.ConnCase, async: true
   import Phoenix.View
 
+  setup %{conn: conn} = config do
+    if config[:test_post] do
+      post = %PhoenixBlog.Post{
+          title: "test post",
+          slug: "test-post", 
+          body: "this is a test.", 
+          published: true, 
+          inserted_at: ~D[2017-01-01]
+        }
+      {:ok, conn: conn, post: post}
+    else
+      :ok
+    end
+  end
+
   test "renders index.html", %{conn: conn} do
     posts = [
       %PhoenixBlog.Post{
@@ -18,7 +33,7 @@ defmodule PhoenixBlog.PostViewTest do
         inserted_at: ~D[2016-01-02]
       }
     ]
-    content = render_to_string(PhoenixBlog.PostView, "index.html", conn: conn, posts: posts, current_user: nil)
+    content = render_to_string(PhoenixBlog.PostView, "index.html", conn: conn, current_user: nil, posts: posts)
     assert String.contains?(content, "Posts")
     for post <- posts do
       assert String.contains?(content, post.title)
@@ -28,8 +43,26 @@ defmodule PhoenixBlog.PostViewTest do
   test "renders new.html", %{conn: conn} do
     changeset = PhoenixBlog.Post.changeset(%PhoenixBlog.Post{})
     content = render_to_string(PhoenixBlog.PostView, "new.html", conn: conn, changeset: changeset)
-
     assert String.contains?(content, "New Post")
+  end
+
+  @tag :test_post
+  test "renders show.html", %{conn: conn, post: post} do
+    content = render_to_string(PhoenixBlog.PostView, "show.html", conn: conn, current_user: nil, post: post)
+    assert String.contains?(content, post.title)
+  end
+
+  @tag :test_post
+  test "renders edit.html", %{conn: conn, post: post} do
+    changeset = PhoenixBlog.Post.changeset(%PhoenixBlog.Post{})
+    content = render_to_string(PhoenixBlog.PostView, "edit.html", conn: conn, changeset: changeset, post: post)
+    assert String.contains?(content, "Edit Post")
+  end
+
+  test "renders form.html", %{conn: conn} do
+    changeset = PhoenixBlog.Post.changeset(%PhoenixBlog.Post{})
+    content = render_to_string(PhoenixBlog.PostView, "form.html", conn: conn, changeset: changeset, action: nil)
+    for word <- ["Title", "Body", "Published"], do: assert String.contains?(content, word)
   end
 
   test "correctly fixes markdown tables" do
