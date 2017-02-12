a732008be4e7249f95b1076d841c8acd80a4df9a
Joe Sweeney <joe@j1.io>
2017-02-12 17:32:05 -0500

    Add unpublished status to latest post on home page

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule PhoenixBlog.PageView do
   use PhoenixBlog.Web, :view
+
+  def modify_post_title(post) do
+    if post.published, do: post, else: Map.put(post, :title, post.title <> " (unpublished)")
+  end
 end
