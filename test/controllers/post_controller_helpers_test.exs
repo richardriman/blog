6458162cead1b23012942c4be949e9528a1b14eb
Joe Sweeney <joe@j1.io>
2017-02-12 16:13:45 -0500

    Test post controller helpers

---
+++ test/controllers/post_controller_helpers_test.exs
@@ -0,0 +1,36 @@
+defmodule PostControllerHelpersTest do
+  use PhoenixBlog.ConnCase
+  import PhoenixBlog.PostControllerHelpers
+
+  test "get_post_list gets only published posts", %{conn: conn} do
+    conn = assign(conn, :current_user, nil)
+    posts = [
+      %{title: "post 1", body: "this is post 1.", published: true},
+      %{title: "post 2", body: "this is post 2.", published: true},
+      %{title: "post 3", body: "this is post 3.", published: false},
+      %{title: "post 4", body: "this is post 4.", published: false}
+    ]
+    for post <- posts do
+      insert_post(post)
+      if post.published do
+        assert Enum.any?(get_post_list(conn), fn p -> p.title == post.title end)
+      end
+    end
+    assert Enum.count(get_post_list(conn)) == 2
+  end
+
+  @tag login_as: "user"
+  test "get_post_list gets all posts when user is logged in", %{conn: conn} do
+    posts = [
+      %{title: "post 1", body: "this is post 1.", published: true},
+      %{title: "post 2", body: "this is post 2.", published: true},
+      %{title: "post 3", body: "this is post 3.", published: false},
+      %{title: "post 4", body: "this is post 4.", published: false}
+    ]
+    for post <- posts do
+      insert_post(post)
+      assert Enum.any?(get_post_list(conn), fn p -> p.title == post.title end)
+    end
+    assert Enum.count(get_post_list(conn)) == 4
+  end
+end
