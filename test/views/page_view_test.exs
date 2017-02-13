6bbf26ce5a8e46d8a1f550ea3d6118e6a40e6f87
Joe Sweeney <joe@j1.io>
2017-02-12 23:53:23 -0500

    Modify view tests to test post published status

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -14,4 +14,18 @@ defmodule PhoenixBlog.PageViewTest do
     assert String.contains?(content, "John Joseph Sweeney")
     assert String.contains?(content, post.title)
   end
+
+  test "renders index.html with unpublished posts", %{conn: conn} do
+    post = %PhoenixBlog.Post{
+        title: "test post",
+        slug: "test-post", 
+        body: "this is a test.", 
+        published: false, 
+        inserted_at: ~D[2017-01-01]
+      }
+    content = render_to_string(PhoenixBlog.PageView, "index.html", conn: conn, latest_post: post)
+    assert String.contains?(content, "John Joseph Sweeney")
+    assert String.contains?(content, post.title)
+    assert String.contains?(content, "#{post.title} (unpublished)")
+  end
 end
--- test/views/post_view_test.exs
+++ test/views/post_view_test.exs
@@ -3,17 +3,18 @@ defmodule PhoenixBlog.PostViewTest do
   import Phoenix.View
 
   setup %{conn: conn} = config do
-    if config[:test_post] do
-      post = %PhoenixBlog.Post{
-          title: "test post",
-          slug: "test-post", 
-          body: "this is a test.", 
-          published: true, 
-          inserted_at: ~D[2017-01-01]
-        }
-      {:ok, conn: conn, post: post}
-    else
-      :ok
+    post = %PhoenixBlog.Post{
+        title: "test post",
+        slug: "test-post", 
+        body: "this is a test.", 
+        published: true, 
+        inserted_at: ~D[2017-01-01]
+      }
+    case config[:test_post] do
+      true -> {:ok, conn: conn, post: post}
+      opts when is_map(opts) ->
+        {:ok, conn: conn, post: Map.merge(post, opts)}
+      _ -> :ok
     end
   end
 
@@ -40,6 +41,33 @@ defmodule PhoenixBlog.PostViewTest do
     end
   end
 
+  test "renders index.html with unpublished posts", %{conn: conn} do
+    posts = [
+      %PhoenixBlog.Post{
+        title: "post 1", 
+        slug: "post-1", 
+        body: "this is post 1.", 
+        published: true, 
+        inserted_at: ~D[2016-01-01]},
+      %PhoenixBlog.Post{
+        title: "post 2", 
+        slug: "post-2", 
+        body: "this is post 2.", 
+        published: false, 
+        inserted_at: ~D[2016-01-02]
+      }
+    ]
+    content = render_to_string(PhoenixBlog.PostView, "index.html", conn: conn, current_user: nil, posts: posts)
+    assert String.contains?(content, "Posts")
+    for post <- posts do
+      if post.published do
+        assert String.contains?(content, post.title)
+      else
+        assert Regex.match?(~r/(#{post.title})(.*)(\(unpublished\))/s, content)
+      end
+    end
+  end
+
   test "renders new.html", %{conn: conn} do
     changeset = PhoenixBlog.Post.changeset(%PhoenixBlog.Post{})
     content = render_to_string(PhoenixBlog.PostView, "new.html", conn: conn, changeset: changeset)
@@ -50,6 +78,12 @@ defmodule PhoenixBlog.PostViewTest do
   test "renders show.html", %{conn: conn, post: post} do
     content = render_to_string(PhoenixBlog.PostView, "show.html", conn: conn, current_user: nil, post: post)
     assert String.contains?(content, post.title)
+  end
+
+  @tag test_post: %{published: false}
+  test "renders show.html with unpublished post", %{conn: conn, post: post} do
+    content = render_to_string(PhoenixBlog.PostView, "show.html", conn: conn, current_user: nil, post: post)
+    assert Regex.match?(~r/(#{post.title})(.*)(\(unpublished\))/s, content)
   end
 
   @tag :test_post
