a20e2d2bd679a47f356f683a017efe740db0fdc2
Joe Sweeney <joe@j1.io>
2017-02-15 01:01:23 -0500

    Test session controller

---
+++ test/controllers/session_controller_test.exs
@@ -0,0 +1,43 @@
+defmodule PhoenixBlog.SessionControllerTest do
+  use PhoenixBlog.ConnCase
+
+  test "shows new session page", %{conn: conn} do
+    conn = get(conn, session_path(conn, :new))
+    assert html_response(conn, :ok) =~ ~r/Login/s
+  end
+
+  test "creates a new session", %{conn: conn} do
+    user = insert_user()
+    params = %{"session" => %{"username" => user.username, "password" => user.password}}
+    conn = post(conn, session_path(conn, :create, params))
+    assert get_flash(conn, :info) == "Welcome back!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+  test "does not create a new session with invalid params", %{conn: conn} do
+    user = insert_user()
+    params = %{"session" => %{"username" => user.username, "password" => "wrong"}}
+    conn = post(conn, session_path(conn, :create, params))
+    assert get_flash(conn, :error) == "Invalid username/password combination"
+    assert html_response(conn, 200)
+  end
+
+  test "creates a new session and correctly sets the session and assigns", %{conn: conn} do
+    user = insert_user()
+    params = %{"session" => %{"username" => user.username, "password" => user.password}}
+    conn = post(conn, session_path(conn, :create, params))
+    assert get_session(conn, :user_id) == user.id
+    assert conn.assigns.current_user == %{user | password: nil}
+  end
+
+  test "logs out of current session", %{conn: conn} do
+    user = insert_user()
+    params = %{"session" => %{"username" => user.username, "password" => user.password}}
+    conn = post(conn, session_path(conn, :create, params))
+    conn = delete(conn, session_path(conn, :delete, conn.assigns.current_user))
+    assert redirected_to(conn) == page_path(conn, :index)
+    refute conn.assigns.current_user == nil
+    next_conn = get(conn, "/")
+    refute get_session(next_conn, :user_id)
+  end
+end
