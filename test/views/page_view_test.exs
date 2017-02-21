a1672c25ca20cafd3dc3a3f72afd4c8f01138eed
Joe Sweeney <joe@j1.io>
2017-02-21 10:40:44 -0500

    Test modify_post_title in page view

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,6 +1,7 @@
 defmodule PhoenixBlog.PageViewTest do
   use PhoenixBlog.ConnCase, async: true
   import Phoenix.View
+  import PhoenixBlog.PageView
 
   test "renders index.html", %{conn: conn} do
     post = %PhoenixBlog.Post{
@@ -27,5 +28,17 @@ defmodule PhoenixBlog.PageViewTest do
     assert String.contains?(content, "John Joseph Sweeney")
     assert String.contains?(content, post.title)
     assert String.contains?(content, "#{post.title} (unpublished)")
+  end
+
+  test "modify_post_title adds unpublished status" do
+    post = %PhoenixBlog.Post{
+        title: "test post",
+        slug: "test-post", 
+        body: "this is a test.", 
+        published: true, 
+        inserted_at: ~D[2017-01-01]
+      }
+    assert modify_post_title(post).title == post.title
+    assert modify_post_title(Map.merge(post, %{published: false})).title == post.title <> " (unpublished)"
   end
 end
