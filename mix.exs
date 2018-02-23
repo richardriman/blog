845c4d64ff7a12dfabd023f01d02c63dc05bbea8
Joe Sweeney <joe@j1.io>
2018-02-22 21:41:20 -0500

    Fix some Docker env variable config bugs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blog.Mixfile do
     [
       app: :blog,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       build_embedded: Mix.env == :prod,
