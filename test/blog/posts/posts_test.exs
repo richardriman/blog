1ee568bb2f3e46e7c659f034ae12853a53a184e7
Joe Sweeney <joe@j1.io>
2018-03-10 18:57:53 -0500

    Clarify post context tests

--- test/blog/posts/posts_test.exs
+++ test/blog/posts/posts_test.exs
@@ -7,18 +7,18 @@ defmodule Blog.PostsTest do
   @valid_attrs post_valid_attrs()
   @invalid_attrs post_invalid_attrs()
 
-  test "list_posts/0 lists all posts" do
+  test "list_posts/0 lists all posts in order of insertion date" do
     posts = gen_post_fixtures(4)
     assert Posts.list_posts() == posts
   end
-  
-  test "list_published_posts/0 lists all published posts" do
+
+  test "list_published_posts/0 lists all published posts in order of insertion date" do
     posts = gen_post_fixtures(4)
 
     result = Posts.list_published_posts()
-    published = Enum.filter(posts, fn p -> p.published == true end) 
+    published = Enum.filter(posts, fn p -> p.published == true end)
     unpublished = Enum.filter(posts, fn p -> p.published == false end)
-    
+
     assert Posts.list_published_posts() == published
 
     for post <- unpublished do
@@ -45,7 +45,7 @@ defmodule Blog.PostsTest do
 
     test "with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Posts.create_post(@invalid_attrs)
-    end 
+    end
 
     test "with valid data creates posts with valid slugs" do
       attrs_list = [
@@ -55,8 +55,8 @@ defmodule Blog.PostsTest do
         %{title: "123test_post%^&*", body: "this is a test post.", published: true}
       ]
 
-      for attrs <- attrs_list do 
-        {:ok, %Post{} = new_post} = Posts.create_post(attrs) 
+      for attrs <- attrs_list do
+        {:ok, %Post{} = new_post} = Posts.create_post(attrs)
         assert new_post.slug == Slugger.slugify_downcase(attrs.title)
       end
     end
@@ -64,11 +64,11 @@ defmodule Blog.PostsTest do
     test "converts unique_constraint on slug to error" do
       post_fixture(%{title: "a cool post", body: "this is a cool post.", published: true})
       attrs = Map.put(@valid_attrs, :title, "a cool post")
-      assert {:error, %Ecto.Changeset{} = changeset} = Posts.create_post(attrs) 
+      assert {:error, %Ecto.Changeset{} = changeset} = Posts.create_post(attrs)
       assert {:slug, {"There was a problem generating a unique slug for this post. Please ensure that this title is not already taken.", []}} in changeset.errors
     end
   end
-  
+
   describe "update_post/2" do
     @update_attrs %{title: "updated title"}
 
@@ -85,7 +85,7 @@ defmodule Blog.PostsTest do
     test "with invalid data returns error changeset", %{post: post} do
       assert {:error, %Ecto.Changeset{}} = Posts.update_post(post, @invalid_attrs)
       assert post == Posts.get_post_by_slug!(post.slug)
-    end 
+    end
 
     test "with valid data updates posts with valid slugs", %{post: post} do
       attrs_list = [
@@ -95,8 +95,8 @@ defmodule Blog.PostsTest do
         %{title: "123test_post%^&*"}
       ]
 
-      for attrs <- attrs_list do 
-        {:ok, updated_post} = Posts.update_post(post, attrs) 
+      for attrs <- attrs_list do
+        {:ok, updated_post} = Posts.update_post(post, attrs)
         assert %Post{} = updated_post
         assert updated_post.slug == Slugger.slugify_downcase(attrs.title)
       end
@@ -104,7 +104,7 @@ defmodule Blog.PostsTest do
 
     test "converts unique_constraint on slug to error", %{post: post} do
       new_post = post_fixture(%{title: "a cool post"})
-      assert {:error, %Ecto.Changeset{} = changeset} = Posts.update_post(new_post, %{title: post.title}) 
+      assert {:error, %Ecto.Changeset{} = changeset} = Posts.update_post(new_post, %{title: post.title})
       assert {:slug, {"There was a problem generating a unique slug for this post. Please ensure that this title is not already taken.", []}} in changeset.errors
     end
   end
@@ -118,5 +118,5 @@ defmodule Blog.PostsTest do
   test "change_post/1 returns a post changeset" do
     post = post_fixture()
     assert %Ecto.Changeset{} = Posts.change_post(post)
-  end 
+  end
 end
