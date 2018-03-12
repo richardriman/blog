09a9c52047c096e574b7f0ba5f1cb160455d56af
Joe Sweeney <joe@j1.io>
2018-03-11 19:16:55 -0600

    Test layout view title/3

--- test/blog_web/views/layout_view_test.exs
+++ test/blog_web/views/layout_view_test.exs
@@ -1,3 +1,28 @@
 defmodule BlogWeb.LayoutViewTest do
   use BlogWeb.ConnCase, async: true
+  import BlogWeb.LayoutView
+
+  @default_title "Joe Sweeney"
+
+  defmodule ViewModuleWithoutFunction do
+    def foo(), do: :bar
+  end
+
+  defmodule ViewModuleWithoutClause do
+    def title("index.html", _assigns), do: "Page"
+  end
+
+  describe "title/3" do
+    test "returns title from view module title/2" do
+      assert title(ViewModuleWithoutClause, "index.html", %{}) == "Page | Joe Sweeney"
+    end
+
+    test "returns default title when view module doesn't implement title/2" do
+      assert title(ViewModuleWithoutFunction, "show.html", %{}) == @default_title
+    end
+
+    test "returns default title when view module doesn't implement correct title/2 clause" do
+      assert title(ViewModuleWithoutClause, "show.html", %{}) == @default_title
+    end
+  end
 end
